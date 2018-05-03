641b44c131ed6d38dcb4d931714d4966e7fb6939
Ali <aliahansraj@gmail.com>
2018-05-03 19:16:24 +0100

    Simple program to withdraw money

---
+++ Account.py
@@ -0,0 +1,7 @@
+Balance = int(500)
+print('Your baalance is :' + str(Balance)) 
+transaction = input('How much would you like to withdraw ')
+transaction = int(transaction)
+print (Balance)
+Balance = Balance - transaction
+print(Balance)
