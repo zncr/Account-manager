28c30ca558aaf1b28f3df8770b22cbadafb427e7
Ali <aliahansraj@gmail.com>
2018-05-04 19:16:05 +0100

    added error message for withdrawls more than balance

--- Account.py
+++ Account.py
@@ -1,7 +1,9 @@
 Balance = int(500)
-print('Your baalance is :' + str(Balance)) 
+print('Your balance is :' + str(Balance)) 
 transaction = input('How much would you like to withdraw ')
 transaction = int(transaction)
-print (Balance)
-Balance = Balance - transaction
-print(Balance)
+if transaction > Balance:
+    print("You can't withdraw more than your balance")
+else:   Balance = Balance - transaction  
+
+print('Your current balance is: ' + str(Balance))
